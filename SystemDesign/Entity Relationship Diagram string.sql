9da7257daf79be537834b3ecca064e60fa522299
Wang <gavin19950511@gmail.com>
2018-01-04 23:48:38 +0800

    update

--- SystemDesign/Entity Relationship Diagram string.sql
+++ SystemDesign/Entity Relationship Diagram string.sql
@@ -1,10 +1,10 @@
 (user_list       :user_id)
-(topic_list      :topic_id,user_id)
+(topic_list      :topic_id,user_id),content
 (activity_list   :avtivity_id,topic_id,user_id,assessment_id)
-(assessment_list :assessment_id,user_id)
+(assessment_list :assessment_id,user_id),assessment_link
 (tester_list     :tester_id,user_id,avtivity_id,chatroom_id,group)
 (chatroom_list   :chatroom_id,avtivity_id)
-(actionDoc       :actionDoc_id)
+(actionDoc       :actionDoc_id),content
 (action_list     :action_id,actionDoc_id,tester_id),exe_time,
 
 
