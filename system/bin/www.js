e2abd0a0266f25faabca419c36375e8facfbbf9f
gavin <f106524018@g.ncu.edu.tw>
2018-04-17 18:45:19 +0800

    v3

--- system/bin/www.js
+++ system/bin/www.js
@@ -17,11 +17,57 @@ io.path(project_root_link);
 
 
 io.on('connection', function(socket){
-    socket.on('Push_message', function(msg,roomID,color_code){
+	var controller_of_action_list = require('../routes/Controller/action_list.js');
+    socket.on('Push_message', function(tester_id,msg,roomID,color_code,actionDoc_id,last_time){
         console.log('In roomID:'+roomID);
+        console.log(tester_id);
         console.log(msg);
-        io.emit(roomID+'_upload_message', msg ,color_code);
+        console.log(actionDoc_id);
+        console.log(last_time);
+        io.in(String(roomID)).emit(roomID+'_upload_message', msg ,color_code);
+        var c = new controller_of_action_list();
+
+    
+	    c.controller(actionDoc_id, tester_id, msg, last_time, function(respond) {
+	    	console.log(respond);
+	    });
+	    delete c ;
+
+
+    });
+    socket.on('login', function(msg,roomID,color_code){
+    	socket.join(String(roomID));
+        console.log('join roomID:'+roomID);
+        console.log(msg);
+        io.in(String(roomID)).emit(roomID+'_upload_message', msg ,color_code);
     });
     
 });
 
+
+
+/*
+var actionDoc_id = req.param('ad_id', null);
+    var tester_id = req.param('t_id', null);
+    var content = req.param('content', null);
+    var last_time = req.param('l_t', null);
+
+    // initialize controller
+    var controller_of_action_list = require('./Controller/action_list.js');
+    c = new controller_of_action_list();
+
+    
+    c.controller(actionDoc_id, tester_id, content, last_time, function(respond) {
+        if (req.param('host_id_API', null)) {
+            // if in testing, respond json data
+            res.end("" + JSON.stringify(respond));
+
+        } else {
+            console.log(respond);
+            //res.render('argument/errorpage', { error_id: '#200', error_con: respond["message"] });
+
+        }
+
+    });
+
+ */
